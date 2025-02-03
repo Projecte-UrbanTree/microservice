bd03c53258b779a2a50af60051e403688d04ede4
Oriol Reverte <oreverte@avanzaprl.es>
2025-02-03 18:21:58 +0000

    refactor: remove unused import of StaticFiles in main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.v1.endpoints import sensors, test_db
-
 
 def create_app() -> FastAPI:
     app = FastAPI()
