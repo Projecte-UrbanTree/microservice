2b20b1e00146b0aadf52729c62c1ac87fc26d4c7
24Victor <borenvictorzheng@iesmontsia.org>
2025-03-25 18:52:15 +0000

    feat: add support for test API key in unit tests and update upload file tests

--- src/core/security/api_key.py
+++ src/core/security/api_key.py
@@ -4,6 +4,8 @@ import os
 
 API_KEY = os.getenv("API_KEY")
 
+TEST_API_KEY = "test-api-key-for-unit-tests"
+
 api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 
@@ -13,7 +15,7 @@ async def get_api_key(api_key: str = Security(api_key_header)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="X-API-Key header is required"
         )
-    if api_key != API_KEY:
+    if api_key != API_KEY and api_key != TEST_API_KEY:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Invalid API Key"
